735f7cf923923a692da5f1a614f5572a869ed514
lakshmannarale01 <lakshmannarale@gmail.com>
2026-01-01 18:15:58 +0530

    created new structure but its incomplete

--- Hotel_management_frontend/HotelManagemnt_frontend/src/app/app.routes.server.js
+++
@@ -1,8 +0,0 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
---
+++ hotel-frontend/src/app/app.component.js
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+  title = 'hotel-frontend';
+  username = '';
+
+  register() {
+    const user = {
+      username: this.username
+    };
+    console.log('Register:', user);
+    alert('Sent to Spring Boot: ' + JSON.stringify(user));
+  }
+}
