0d748e747cb6cb167d3501caa0cc1481e4c9e0a4
lakshmannarale01 <lakshmannarale@gmail.com>
2026-01-02 14:23:45 +0530

    building first service stage 6

--- hotel-frontend/src/app/user-register/user-register.component.js
+++ hotel-frontend/src/app/user-register/user-register.component.js
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule ,httpclient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-register',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule , CommonModule ],
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
