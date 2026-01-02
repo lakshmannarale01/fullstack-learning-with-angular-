1c73d778348320601b59ec1ee6e14ed0fb735f20
lakshmannarale01 <lakshmannarale@gmail.com>
2026-01-02 21:25:58 +0530

    building first service stage 8

---
+++ Hotel-frontend/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) }
+];
+
---
+++ Hotel-frontend/src/main.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck  // Ignore TS errors
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
